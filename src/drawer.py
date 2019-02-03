7928ee8f13ee3d588a1a4c445295a657a012e83a
xia-st <xia@369278@gmail.com>
2019-02-03 08:25:11 +0000

    update some code

--- src/drawer.py
+++ src/drawer.py
@@ -1,36 +1,56 @@
+from ev3dev2.motor import MediumMotor, LargeMotor, SpeedPercent
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C
 class Pen:
     def __init__(self):
+        self.__udm = MediumMotor(OUTPUT_B) # up and down motor
+        self.__lrm = LargeMotor(OUTPUT_A)
         self.isUp = False
+        self.loading = False
         self.speed = 1
+        self.ll = 0
+        self.lr = 5
         pass
 
     def up(self):
         # 抬起笔
-        if self.isUp:
+        print('up', self.isUp)
+        if self.loading or self.isUp:
             return
 
+        self.__udm.on_for_rotations(SpeedPercent(75), 1)
         self.isUp = True
-        pass
 
     def down(self):
         # 放下笔
-        if not self.isUp:
+        print('down', self.isUp)
+        if self.loading or not self.isUp:
             return
 
+        self.__udm.on_for_rotations(SpeedPercent(75), -1)
         self.isUp = False
         pass
+
+    def load(self):
+        if self.loading:
+            return
+
+        self.loading = True
+        self.__udm.on_for_rotations(SpeedPercent(75), 4)
+
+    def loaded(self):
+        if not self.loading:
+            return
+
+        self.__udm.on_for_rotations(SpeedPercent(75), -4)
+        self.loading = False
 
     def adjustUpDown(self, angle):
         # 调整放下笔的角度，angle正数为放下，负数为抬起
         pass
 
-    def waitUpDown(self):
-        # 等待抬起放下操作结束
-        pass
-
-    def move(self, d, speed):
+    def move(self, d):
         # 笔移动距离d，正数向右，负数向左
-        pass
+        self.__lrm.on_for_rotations(SpeedPercent(75), d, block=False)
 
     def moveToLeft(self):
         # 移动笔的位置到最左边，用于结束绘制后移动到最左边
@@ -38,7 +58,7 @@ class Pen:
 
     def waitMove(self):
         # 等待笔左移右移结束
-        pass
+        self.__lrm.wait_until_not_moving()
 
 class Paper:
     def __init__(self):
@@ -60,3 +80,8 @@ class Paper:
     def waitScroll(self, d):
         # 等待滚动完成
         pass
+
+if __name__ == '__main__':
+    pen = Pen()
+    pen.move(-0.2)
+    pen.waitMove()
